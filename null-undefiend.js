2e763f2458abaf4c976e1eaa87100347fd6e5b1e
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-03 20:03:31 +0600

    null vs undefiend

---
+++ null-undefiend.js
@@ -0,0 +1,27 @@
+//undefined ;
+
+let jeKew; 
+
+function add(num1, num2) {
+    console.log(num1 +num2);
+    return
+}
+
+function add(num1, num2){
+    // console.log(num1, num2);
+    return num1, num2;
+}
+const result = add(13);
+console.log(result);
+
+const sowmik = {name: 'smart dude', phone: '909032'};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3,2,23,23];
+console.log(ages[11]);
+
+
+//null mean's not exesting;
--- truthy-falsy.js
+++ truthy-falsy.js
@@ -1,1 +1,24 @@
-// Advanced javascrip future 
+//Falsy:
+//0 ;
+//"" ;
+//undefined;
+//null;
+//NaN;
+//false;
+
+//truthy:
+// '0';
+// " ";
+//[];
+//{};
+//"false";
+
+
+const age = 0/3;
+
+if (age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
