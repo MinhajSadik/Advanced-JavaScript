8db19eee0fe5d049a1c8a0e42e1914c5695059a1
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-03 20:21:59 +0600

    equal how to works

---
+++ equalChack.js
@@ -0,0 +1,51 @@
+// thripple equal chack
+
+const frist = 2;
+const second = 3;
+if(first == second){// dubble equal chack value 
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
+// answer is ture
+
+const frist1 = 2;
+const second1 = "3";
+if (first1 === second1) {//tripple equal mean's chack of value and type 
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
+//answer is false
+
+const frist2 = 1; //1 is positive value
+const second2 = true;
+if (first2 == second2) {//tripple equal mean's chack of value and type 
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
+// answer is true;
+
+const frist2 = 0; // 0 is negative value
+const second2 = true;
+if (first2 == second2) {//tripple equal mean's chack of value and type 
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
+//answer is false
+
+const frist2 = 0; //negative + negative = positive;
+const second2 = false;
+if (first2 == second2) {//tripple equal mean's chack of value and type 
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
+// answer is true
