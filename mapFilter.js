d6c0f24e86753748017820e47d482721d754e7ab
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-03 22:35:20 +0600

    Map And Filter And Find

---
+++ mapFilter.js
@@ -0,0 +1,35 @@
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+// const output = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+
+// }
+// console.log(output);
+
+
+// function square(element) {
+//     return element * element;
+// }
+
+// const square = element => element * element; 
+// const result = numbers.map(x => x * x);
+// const square1 = x => x * x;
+
+
+// numbers.map(function(element, index, array){// map also return an array
+//     console.log(element, index, array);
+// })
+
+// const result = numbers.map(function(element){  
+//     return element * element;
+// })
+// console.log(result);
+
+const bigger = numbers.filter(x => x < 5); // filter also return an array
+console.log(bigger);
+const isThere = numbers.find(x => x > 5); // find also return an element 
+console.log(isThere);
