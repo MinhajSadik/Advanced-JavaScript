223063f1fbf25252695c0f97735c3d0674a75779
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-04 16:37:53 +0600

    learn closure

--- closure.js
+++ closure.js
@@ -1,3 +1,4 @@
+
 function stopWatch() {
     let count = 0;
     return function(){
@@ -18,3 +19,10 @@ console.log(clock2()); 3
 console.log(clock1()); 5
 console.log(clock1()); 6
 
+
+
+/*closer: hocce akta function er bitor teke jodi aro ekta function ke call kora hoy
+tahole tara nijeder bitore ekta close environment create kore and jokon function ta
+kuno kisu return or call kora hobe tokon tader nijosso ekekta value take and oi value 
+ta return kore.
+*/
